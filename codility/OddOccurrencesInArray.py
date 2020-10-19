92636f358df90bfec039f85d47dc8f200d0bee35
Vargas <a.vargas.pina@gmail.com>
2020-10-19 11:04:33 +0200

    renaming

--- codility/OddOccurrencesInArray.py
+++
@@ -1,6 +0,0 @@
-def solution(A):
-    # write your code in Python 3.6
-    for e in A:
-        if A.count(e)==1:
-            return e
-        
--- codility/binary_gap.py
+++
@@ -1,4 +0,0 @@
-# N integer, get the binary gap
-def solution(N):
-    grouped_0s=format(N,'b').strip('0').split("1")
-    return len(max(grouped_0s))
