f3ceec9e0a69de5660d70c96e7aeaba03cdc48be
Vargas <a.vargas.pina@gmail.com>
2020-10-27 10:31:45 +0100

    daily kata

---
+++ codewars-6kyu/find_the_parity_outlier.py
@@ -0,0 +1,17 @@
+# Find The Parity Outlier
+# https://www.codewars.com/kata/5526fc09a1bbd946250002dc/train/python
+
+def find_outlier(integers):
+    even=[]
+    odd=[]
+    for i in integers:
+        if i%2==0:
+            even.append(i)
+        else:
+            odd.append(i)
+    
+    if len(even)<len(odd):
+        return even[0]
+    else:
+        return odd[0]
+
