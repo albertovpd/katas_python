331690b972df08b111c7a740ff5bbe3198b270e9
Vargas <a.vargas.pina@gmail.com>
2021-04-13 11:08:43 +0200

    blocked by timeout

---
+++ codewars-4kyu/next_bigger_number.py
@@ -0,0 +1,10 @@
+# timeout
+
+# from itertools import permutations
+
+# def next_bigger(n):
+#     numbers = list(str(n))
+#     num_permutations = sorted(set([int(''.join(num)) for num in permutations(numbers, len(numbers))]))
+    
+#     next_number = num_permutations[num_permutations.index(n)+1]
+#     return  next_number if next_number > n else -1
